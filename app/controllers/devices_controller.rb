7786f627b5b457890a13bcc9b51e60a3dbe376d6
John <johnrmichaelsjr@gmail.com>
2019-12-10 19:15:14 -0700

    Update devices_controller.rb

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,9 +1,22 @@
 class DevicesController < ApplicationController
   def index
-    @devices = Device.all
+    respond_to do |format|
+      format.html do
+        @devices = Device.all
+      end
+
+      format.json do
+        @devices = Device.paginate(page: params[:page], per_page: 50)
+        response = {
+          returned_records: @devices.count,
+          results: @devices
+        }
+        render json: response.to_json
+      end
+    end
   end
 
   def show
     @device = Device.find(params[:id])
-  end
+  end 
 end
